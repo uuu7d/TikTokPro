d9a8997d2a28294f022763f32aa2f20eacba26d9
Raed <uu77dd@icloud.com>
2025-12-30 14:23:14 +0300

    Update VCAMGlobals.h and VCAMGlobals.m

--- VCAMGlobals.h
+++ VCAMGlobals.h
@@ -1,12 +1,10 @@
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
+#import <UIKit/UIKit.h>
 
 extern BOOL vcamEnabled;
 extern BOOL vcamMirror;
-
 extern NSString *vcamVideoPath;
 extern NSString *vcamImagePath;
-
 extern AVSampleBufferDisplayLayer *vcamPreview;
 extern UIVisualEffectView *vcamBlur;
--- VCAMGlobals.m
+++ VCAMGlobals.m
@@ -2,9 +2,7 @@
 
 BOOL vcamEnabled = NO;
 BOOL vcamMirror = NO;
-
 NSString *vcamVideoPath = @"/var/mobile/Library/Caches/vcam.mov";
 NSString *vcamImagePath = @"/var/mobile/Library/Caches/vcam.jpg";
-
 AVSampleBufferDisplayLayer *vcamPreview = nil;
 UIVisualEffectView *vcamBlur = nil;
